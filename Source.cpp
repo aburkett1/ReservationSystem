d2b5b746fd4f0c70e9a38e2026b1874000d0f65a
Austin <austintburkett@gmail.com>
2025-12-14 16:53:43 -0800

    Added menus

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,86 @@
 #include "Menu.h"
 #include "Utilities.h"
+#include "ReservationSystem.h"
 
+// ==== main ===================================================================
+//
+// =============================================================================
+int main()
+{
+    // Variables
+    int selection{};
+    ReservationSystem reservationSystem = ReservationSystem();
+    ifstream fin;
+    ofstream fout;
+
+    // =============================================================================
+    // MARK: Menu Creation
+    // =============================================================================
+
+    vector<Menu> menus = setupMenus();
+    Menu mainMenu = menus[0];
+    Menu loginMenu = menus[1];
+    Menu studentMenu = menus[2];
+    Menu adminMenu = menus[3];
+    Menu viewResourcesMenu = menus[4];
+    Menu reservationCreationMenu = menus[5];
+    Menu viewReservationsMenu = menus[6];
+    Menu modifyResourceMenu = menus[7];
+    
+    /*
+        // =========================================================================
+        // MARK: MENU HIERARCHY
+        // =========================================================================
+    
+        MENU HIERARCHY
+
+        Main Menu
+            Load System Details
+                Login
+                    if (Student)
+                        View Resources
+                            List all
+                                Select Resource
+                                    Create Reservation
+                            Search by ID
+                                Select Resource
+                                    Create Reservation
+                            Search by Name
+                                Select Resource
+                                    Create Reservation
+                            Filter By Resource Type
+                                Select Resource
+                                    Create Reservation
+                        View Reservations
+                            Modify Reservation
+                            Cancel Reservation
+                        Save System Details
+                    if (Admin)
+                        Add Resource
+                        View Resources
+                            List all
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                            Search by ID
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                            Search by Name
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                            Filter By Resource Type
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                        Save System Details
+                Register
+            New System
+            
+    */
+
+
+    return 0;
+} // end of main()
+// =============================================================================
--- Utilities.cpp
+++ Utilities.cpp
@@ -11,10 +11,119 @@ vector<Menu> setupMenus()
 
     /*
         MENU HIERARCHY
-    
+
+        Main Menu
+            Load System Details
+                Login
+                    if (Student)
+                        View Resources
+                            List all
+                                Select Resource
+                                    Create Reservation
+                            Search by ID
+                                Select Resource
+                                    Create Reservation
+                            Search by Name
+                                Select Resource
+                                    Create Reservation
+                            Filter By Resource Type
+                                Select Resource
+                                    Create Reservation
+                        View Reservations
+                            Modify Reservation
+                            Cancel Reservation
+                        Save System Details
+                    if (Admin)
+                        Add Resource
+                        View Resources
+                            List all
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                            Search by ID
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                            Search by Name
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                            Filter By Resource Type
+                                Select Resource
+                                    Edit Resource
+                                    Remove Resource
+                        Save System Details
+                Register
+            New System
         
     */
     
+    // Main Menu
+    vector<string> mainMenuOptions = {
+        "Load System Details",
+        "New System"
+    };
+    Menu mainMenu = Menu("MAIN MENU", mainMenuOptions);
+    menus.push_back(mainMenu);
+
+    // Login Menu
+    vector<string> loginMenuOptions = {
+        "Login",
+        "Register"
+    };
+    Menu loginMenu = Menu("LOGIN MENU", loginMenuOptions);
+    menus.push_back(loginMenu);
+
+    // Student Menu
+    vector<string> studentMenuOptions = {
+        "View Resources",
+        "View Reservations",
+        "Save System Details"
+    };
+    Menu studentMenu = Menu("RESOURCE SYSTEM", studentMenuOptions);
+    menus.push_back(studentMenu);
+
+    // Admin Menu
+    vector<string> adminMenuOptions = {
+        "Add Resource",
+        "View Resources",
+        "Save System Details"
+    };
+    Menu adminMenu = Menu("RESOURCE SYSTEM", adminMenuOptions);
+    menus.push_back(adminMenu);
+
+    // View Resources Menu
+    vector<string> viewResourcesMenuOptions = {
+        "List all",
+        "Search by ID",
+        "Search by Name",
+        "Filter By Resource Type"
+    };
+    Menu viewResourcesMenu = Menu("RESOURCES", viewResourcesMenuOptions);
+    menus.push_back(viewResourcesMenu);
+
+    // Reservation Creation Menu
+    vector<string> reservationCreationMenuOptions = {
+        "Create Reservation"
+    };
+    Menu reservationCreationMenu = Menu("RESOURCE DETAILS", reservationCreationMenuOptions);
+    menus.push_back(reservationCreationMenu);
+
+    // View Reservations Menu
+    vector<string> viewReservationsMenuOptions = {
+        "Modify Reservation",
+        "Cancel Reservation"
+    };
+    Menu viewReservationsMenu = Menu("RESERVATIONS", viewReservationsMenuOptions);
+    menus.push_back(viewReservationsMenu);
+
+    // Modify Resource Menu
+    vector<string> modifyResourceMenuOptions = {
+        "Edit Resource",
+        "Remove Resource"
+    };
+    Menu modifyResourceMenu = Menu("RESOURCE DETAILS", modifyResourceMenuOptions);
+    menus.push_back(modifyResourceMenu);
     
 
     // Return vector of all Menus.
